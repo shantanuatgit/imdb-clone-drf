dbc59589af09d7a95f6e9bc37ff4aaa8e4fc2365
shantanuatgit <shantanu3250@gmail.com>
2022-04-19 15:47:23 +0530

    added user registration & automatic auth_token

--- user_app/api/urls.py
+++ user_app/api/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from rest_framework.authtoken.views import obtain_auth_token
+from user_app.api import views
+
 
 urlpatterns = [
     path('login/', obtain_auth_token, name='login'),
+    path('register/', views.registration_view, name='register'),
+    path('logout/', views.logout_view, name='logout'),
 ]
